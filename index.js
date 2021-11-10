7cd1c9eb9c86e9403346a5945996cc36d5c0d9d2
JazzPants <leighton.l14@gmail.com>
2021-11-10 22:25:35 +1100

    Done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName ="BOB";
+    //return customerName;
+}
+
+//notice that there is no return statement
+  //if you try to use `return customerName.toUpperCase()`, it will return 'BOB', but will not redefine the variable!
+
+//console.log(upperCaseCustomerName()); npm test breaks if this line is ran.
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+//console.log(setBestCustomer()) -> undefined
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+//console.log(overwriteBestCustomer()) // -> undefined
+
+
+const leastFavoriteCustomer = 'jafar'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'aladdin';
+}
+
+//console.log(changeLeastFavoriteCustomer()); -> "TypeError: Assignment to constant variable"
